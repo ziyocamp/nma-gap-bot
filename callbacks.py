65bebe488d78f9466ce8ac1df00855c0a1a994fc
djumanov <djumanovdev@gmail.com>
2025-11-08 09:38:08 +0500

    start command

--- callbacks.py
+++ callbacks.py
@@ -1,7 +1,36 @@
-from telegram import Update
+from telegram import Update, ReplyKeyboardMarkup, KeyboardButton, WebAppInfo
 from telegram.ext import CallbackContext
 
 
 def start(update: Update, context: CallbackContext) -> None:
-    update.message.reply_text('Salom')
+    update.message.reply_text(
+        text=f'Assalomu alaykum {update.message.from_user.first_name}!',
+        reply_markup=ReplyKeyboardMarkup(
+            keyboard=[
+                [
+                    KeyboardButton(
+                        text='🛍 Buyurtma berish',
+                        web_app=WebAppInfo(url='https://uzum.uz')
+                    )
+                ],
+                [
+                    KeyboardButton(
+                        text='📦 Buyurtmalarim'
+                    ),
+                    KeyboardButton(
+                        text='⚙️ Sozlamalar'
+                    )
+                ],
+                [
+                    KeyboardButton(
+                        text='ℹ️ Biz haqimizda'
+                    ), 
+                    KeyboardButton(
+                        text='✍️ Fikr qoldirish'
+                    )
+                ]
+            ],
+            resize_keyboard=True,
+        )
+    )
     
